f9e6d4205c34c9715692117c96a86ba71716df75
sadohpv <anh.trantruong@ncc.asia>
2025-08-25 17:39:22 +0700

    fix cached webhook clan

--- libs/core/src/lib/chat/contexts/ChatContext.tsx
+++ libs/core/src/lib/chat/contexts/ChatContext.tsx
@@ -348,15 +348,15 @@ const ChatContextProvider: React.FC<ChatContextProviderProps> = ({ children }) =
 				const attachmentList: AttachmentEntity[] =
 					message.attachments && message.attachments.length > 0
 						? message.attachments.map((attachment) => {
-							const dateTime = new Date();
-							return {
-								...attachment,
-								id: attachment.url as string,
-								message_id: message?.message_id,
-								create_time: dateTime.toISOString(),
-								uploader: message?.sender_id
-							};
-						})
+								const dateTime = new Date();
+								return {
+									...attachment,
+									id: attachment.url as string,
+									message_id: message?.message_id,
+									create_time: dateTime.toISOString(),
+									uploader: message?.sender_id
+								};
+							})
 						: [];
 
 				if (attachmentList?.length && message?.code === TypeMessage.Chat) {
@@ -1842,7 +1842,7 @@ const ChatContextProvider: React.FC<ChatContextProviderProps> = ({ children }) =
 
 	const oneventwebhook = useCallback(async (webhook_event: ApiWebhook) => {
 		if (webhook_event.status === EEventAction.DELETE) {
-			dispatch(webhookActions.removeOneWebhook({ channelId: webhook_event.channel_id || '', webhookId: webhook_event.id || '' }));
+			dispatch(webhookActions.removeOneWebhook({ clanId: webhook_event.clan_id || '', webhookId: webhook_event.id || '' }));
 		} else {
 			dispatch(webhookActions.upsertWebhook(webhook_event));
 		}
@@ -2211,68 +2211,68 @@ const ChatContextProvider: React.FC<ChatContextProviderProps> = ({ children }) =
 
 		return () => {
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onvoicereactionmessage = () => { };
+			socket.onvoicereactionmessage = () => {};
 
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onchannelmessage = () => { };
+			socket.onchannelmessage = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onchannelpresence = () => { };
+			socket.onchannelpresence = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onnotification = () => { };
+			socket.onnotification = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onpinmessage = () => { };
+			socket.onpinmessage = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.oneventnotiuserchannel = () => { };
+			socket.oneventnotiuserchannel = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onlastseenupdated = () => { };
+			socket.onlastseenupdated = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.oncustomstatus = () => { };
+			socket.oncustomstatus = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onstatuspresence = () => { };
+			socket.onstatuspresence = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.oncanvasevent = () => { };
+			socket.oncanvasevent = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.ondisconnect = () => { };
+			socket.ondisconnect = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onuserchannelremoved = () => { };
+			socket.onuserchannelremoved = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onuserclanremoved = () => { };
+			socket.onuserclanremoved = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onclandeleted = () => { };
+			socket.onclandeleted = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onuserchanneladded = () => { };
+			socket.onuserchanneladded = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onuserclanadded = () => { };
+			socket.onuserclanadded = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onremovefriend = () => { };
+			socket.onremovefriend = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onstickercreated = () => { };
+			socket.onstickercreated = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.oneventemoji = () => { };
+			socket.oneventemoji = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onstickerdeleted = () => { };
+			socket.onstickerdeleted = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onstickerupdated = () => { };
+			socket.onstickerupdated = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onclanprofileupdated = () => { };
+			socket.onclanprofileupdated = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.oncoffeegiven = () => { };
+			socket.oncoffeegiven = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onroleevent = () => { };
+			socket.onroleevent = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onuserstatusevent = () => { };
+			socket.onuserstatusevent = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.oneventwebhook = () => { };
+			socket.oneventwebhook = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.ontokensent = () => { };
+			socket.ontokensent = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onjoinchannelappevent = () => { };
+			socket.onjoinchannelappevent = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onsdtopicevent = () => { };
+			socket.onsdtopicevent = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onunpinmessageevent = () => { };
+			socket.onunpinmessageevent = () => {};
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			socket.onblockfriend = () => { };
+			socket.onblockfriend = () => {};
 		};
 	}, [
 		onchannelmessage,
@@ -2346,4 +2346,3 @@ const ChatContextConsumer = ChatContext.Consumer;
 ChatContextProvider.displayName = 'ChatContextProvider';
 
 export { ChatContext, ChatContextConsumer, ChatContextProvider };
-
