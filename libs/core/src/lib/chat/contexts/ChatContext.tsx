804c11f9e3026434f6d884c0cf2e88111884b6bb
sadohpv <anh.trantruong@ncc.asia>
2025-10-09 15:03:21 +0700

    fix invite badge

--- libs/core/src/lib/chat/contexts/ChatContext.tsx
+++ libs/core/src/lib/chat/contexts/ChatContext.tsx
@@ -409,17 +409,15 @@ const ChatContextProvider: React.FC<ChatContextProviderProps> = ({ children }) =
 
 					if (mess.isMe) {
 						// Mark as read if isMe send token
-						if (mess.code === TypeMessage.SendToken) {
-							dispatch(
-								messagesActions.updateLastSeenMessage({
-									clanId: mess?.clan_id || '',
-									channelId: mess?.channel_id,
-									messageId: mess?.id,
-									mode: mess.mode,
-									badge_count: 0
-								})
-							);
-						}
+						dispatch(
+							messagesActions.updateLastSeenMessage({
+								clanId: mess?.clan_id || '',
+								channelId: mess?.channel_id,
+								messageId: mess?.id,
+								mode: mess.mode,
+								badge_count: 0
+							})
+						);
 						dispatch(directMetaActions.setDirectLastSeenTimestamp({ channelId: message.channel_id, timestamp }));
 					}
 				} else {
