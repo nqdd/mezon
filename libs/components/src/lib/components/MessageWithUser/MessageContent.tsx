68751e549f73b76a19f9fc310e632c15695188a9
trinh.truongthiphuong <trinh.truongthiphuong@ncc.asia>
2025-10-16 11:37:39 +0700

    mark e2e to button view topic

--- libs/components/src/lib/components/MessageWithUser/MessageContent.tsx
+++ libs/components/src/lib/components/MessageWithUser/MessageContent.tsx
@@ -1,7 +1,7 @@
 import { getFirstMessageOfTopic, selectMemberClanByUserId, threadsActions, topicsActions, useAppDispatch, useAppSelector } from '@mezon/store';
 import { Icons } from '@mezon/ui';
 import type { IExtendedMessage, IMessageWithUser } from '@mezon/utils';
-import { EBacktickType, ETypeLinkMedia, addMention, createImgproxyUrl, isValidEmojiData } from '@mezon/utils';
+import { EBacktickType, ETypeLinkMedia, addMention, createImgproxyUrl, generateE2eId, isValidEmojiData } from '@mezon/utils';
 import { safeJSONParse } from 'mezon-js';
 import React, { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -69,6 +69,7 @@ export const TopicViewButton = ({ message }: { message: IMessageWithUser }) => {
 		<div
 			className=" border-theme-primary  text-theme-primary bg-item-theme text-theme-primary-hover rounded-lg my-1 p-1 w-[70%] flex justify-between items-center cursor-pointer group/view-topic-btn "
 			onClick={handleOpenTopic}
+			data-e2e={generateE2eId('chat.topic.button.view_topic')}
 		>
 			<div className="flex items-center gap-2 text-sm h-fit flex-1 min-w-0">
 				<AvatarImage
