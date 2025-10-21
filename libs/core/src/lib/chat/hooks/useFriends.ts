90b2afe557c9210a54916af55e71dda3056cbe3b
thin.tranhoang <thin.tranhoang@ncc.asia>
2025-10-21 08:51:28 +0700

    update clean code

--- libs/core/src/lib/chat/hooks/useFriends.ts
+++ libs/core/src/lib/chat/hooks/useFriends.ts
@@ -17,7 +17,7 @@ export function useFriends() {
 	const friends = useSelector(selectAllFriends);
 	const currentDM = useSelector(selectDmGroupCurrentId);
 	const groupDmMember = useAppSelector((state) => selectMemberByGroupId(state, currentDM as string));
-	const numberMemberInDmGroup = useMemo(() => groupDmMember?.length, [groupDmMember]);
+	const numberMemberInDmGroup = useMemo(() => groupDmMember?.length || 0, [groupDmMember]);
 	const currentUserId = useSelector(selectCurrentUserId);
 	const dispatch = useAppDispatch();
 
@@ -105,7 +105,7 @@ export function useFriends() {
 			if (isAddMember) {
 				return friends.filter((friend) => {
 					if (friend.user?.display_name?.toUpperCase().includes(searchTerm) || friend.user?.username?.toUpperCase().includes(searchTerm)) {
-						if (!Object.values(groupDmMember ?? [])?.some((user) => user.id === friend.id)) {
+						if (!Object.values(groupDmMember ?? [])?.some((user) => user?.id === friend?.id)) {
 							return friend;
 						}
 					}
