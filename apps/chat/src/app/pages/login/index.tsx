20db3e3fd603412e3058adf9e1709d7018738c2c
thin.tranhoang <thin.tranhoang@ncc.asia>
2025-10-23 21:51:46 +0700

    update: UI form login desktop

--- apps/chat/src/app/pages/login/index.tsx
+++ apps/chat/src/app/pages/login/index.tsx
@@ -82,28 +82,28 @@ function Login() {
 		<div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 via-purple-500 to-pink-300 px-4">
 			<div className="bg-[#0b0b0b] text-white rounded-2xl shadow-lg p-14 max-w-4xl w-[800px] flex flex-row gap-8 items-center">
 				<div className="flex-1 text-left flex flex-col">
-					<div className="flex flex-col items-center">
+					<div className="flex flex-col items-center mb-2">
 						<h1 className="text-2xl font-bold mb-1">{t('login.welcomeBack')}</h1>
 						<p className="text-gray-400">{t('login.gladToMeetAgain')}</p>
 					</div>
 					{loginMethod ? <FormLoginOTP handleChangeMethod={handleSwitchMethod} /> : <FormLoginEmail />}
-					<div className="mt-4 flex items-center text-gray-400 justify-between">
-						<div className="flex items-center gap-2">
-							<input
-								type="checkbox"
-								id="keepSignedIn"
-								className="mr-2"
-								checked={isRemember}
-								onChange={(e) => setIsRemember(e.target.checked)}
-							/>
-							<label htmlFor="keepSignedIn">{t('login.keepSignedIn')}</label>
-						</div>
-						{!loginMethod && (
+					{!loginMethod && (
+						<div className="mt-4 flex items-center text-gray-400 justify-between">
+							<div className="flex items-center gap-2">
+								<input
+									type="checkbox"
+									id="keepSignedIn"
+									className="mr-2"
+									checked={isRemember}
+									onChange={(e) => setIsRemember(e.target.checked)}
+								/>
+								<label htmlFor="keepSignedIn">{t('login.keepSignedIn')}</label>
+							</div>
 							<div className="text-sm text-blue-500 hover:underline" onClick={handleSwitchMethod}>
 								{t('login.loginByOTP')}
 							</div>
-						)}
-					</div>
+						</div>
+					)}
 				</div>
 
 				<div className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center w-56 h-80">
