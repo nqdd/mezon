f64447ab6ce93799d766e92337a7994b6d069d34
sadohpv <anh.trantruong@ncc.asia>
2025-10-27 17:36:59 +0700

    return null

--- apps/chat/src/app/pages/channel/ChannelMedia.tsx
+++ apps/chat/src/app/pages/channel/ChannelMedia.tsx
@@ -35,7 +35,7 @@ export const ChannelMedia = ({ currentChannel }: ChannelMediaProps) => {
 		);
 	}
 
-	return ChannelMessages?.Skeleton ? <ChannelMessages.Skeleton /> : null;
+	return null;
 };
 
 type KeyPressListenerProps = {
