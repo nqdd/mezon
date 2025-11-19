e895efd023a084e99dafbf0a6efe7a80df03782b
hoangtranlehuy <hoang.tranlehuy@ncc.asia>
2025-11-19 13:13:50 +0700

    feat show action remove clan avatar mobile

--- apps/mobile/src/app/components/ClanSettings/LogoClanSelector/LogoClanSelector.tsx
+++ apps/mobile/src/app/components/ClanSettings/LogoClanSelector/LogoClanSelector.tsx
@@ -1,14 +1,16 @@
 import { usePermissionChecker } from '@mezon/core';
-import { useTheme } from '@mezon/mobile-ui';
+import { size, useTheme } from '@mezon/mobile-ui';
 import { clansActions, selectCurrentClan } from '@mezon/store';
 import { useAppDispatch } from '@mezon/store-mobile';
 import { EPermission, MAX_FILE_SIZE_1MB } from '@mezon/utils';
 import { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import Toast from 'react-native-toast-message';
 import { useSelector } from 'react-redux';
+import MezonIconCDN from '../../../componentUI/MezonIconCDN';
 import MezonImagePicker from '../../../componentUI/MezonImagePicker';
+import { IconCDN } from '../../../constants/icon_cdn';
 import { style } from './style';
 
 const LogoClanSelector = () => {
@@ -17,18 +19,31 @@ const LogoClanSelector = () => {
 	const currentClan = useSelector(selectCurrentClan);
 	const dispatch = useAppDispatch();
 	const { t } = useTranslation(['clanSetting']);
-	const [hasAdminPermission, hasManageClanPermission, clanOwnerPermission] = usePermissionChecker([
-		EPermission.administrator,
-		EPermission.manageClan,
-		EPermission.clanOwner
-	]);
-	const isHavePermission = useMemo(() => {
-		return hasAdminPermission || hasManageClanPermission || clanOwnerPermission;
-	}, [clanOwnerPermission, hasAdminPermission, hasManageClanPermission]);
+	const [hasAdminPermission, clanOwnerPermission] = usePermissionChecker([EPermission.administrator, EPermission.clanOwner]);
+
+	const isHavePermission = useMemo(() => hasAdminPermission || clanOwnerPermission, [clanOwnerPermission, hasAdminPermission]);
+
+	const updateClanLogo = useCallback(
+		(logo: string) =>
+			dispatch(
+				clansActions.updateClan({
+					clan_id: currentClan?.clan_id ?? '',
+					request: {
+						banner: currentClan?.banner ?? '',
+						clan_name: currentClan?.clan_name ?? '',
+						creator_id: currentClan?.creator_id ?? '',
+						is_onboarding: currentClan?.is_onboarding,
+						logo,
+						welcome_channel_id: currentClan?.welcome_channel_id ?? ''
+					}
+				})
+			),
+		[currentClan, dispatch]
+	);
 
 	const handleLoad = useCallback(
 		async (url?: string) => {
-			if (!hasAdminPermission && !clanOwnerPermission) {
+			if (!isHavePermission) {
 				Toast.show({
 					type: 'error',
 					text1: t('menu.settings.permissionDenied')
@@ -36,35 +51,22 @@ const LogoClanSelector = () => {
 				return;
 			}
 			if (url) {
-				await dispatch(
-					clansActions.updateClan({
-						clan_id: currentClan?.clan_id ?? '',
-						request: {
-							banner: currentClan?.banner ?? '',
-							clan_name: currentClan?.clan_name ?? '',
-							creator_id: currentClan?.creator_id ?? '',
-							is_onboarding: currentClan?.is_onboarding,
-							logo: url || (currentClan?.logo ?? ''),
-							welcome_channel_id: currentClan?.welcome_channel_id ?? ''
-						}
-					})
-				);
+				await updateClanLogo(url);
 			}
 		},
-		[
-			clanOwnerPermission,
-			currentClan?.banner,
-			currentClan?.clan_id,
-			currentClan?.clan_name,
-			currentClan?.creator_id,
-			currentClan?.is_onboarding,
-			currentClan?.logo,
-			currentClan?.welcome_channel_id,
-			dispatch,
-			hasAdminPermission,
-			t
-		]
+		[isHavePermission, t, updateClanLogo]
 	);
+
+	const handleRemoveClanAvatar = useCallback(async () => {
+		if (!isHavePermission) {
+			Toast.show({
+				type: 'error',
+				text1: t('menu.settings.permissionDenied')
+			});
+			return;
+		}
+		await updateClanLogo('');
+	}, [isHavePermission, t, updateClanLogo]);
 
 	return (
 		<View style={styles.logoSection}>
@@ -77,8 +79,16 @@ const LogoClanSelector = () => {
 					disabled={!isHavePermission}
 					imageSizeLimit={MAX_FILE_SIZE_1MB}
 				/>
+				{isHavePermission && currentClan?.logo && (
+					<TouchableOpacity
+						disabled={!isHavePermission}
+						style={{ position: 'absolute', top: -size.s_6, right: -size.s_6 }}
+						onPress={handleRemoveClanAvatar}
+					>
+						<MezonIconCDN icon={IconCDN.circleXIcon} color={themeValue.text} />
+					</TouchableOpacity>
+				)}
 			</View>
-
 			<Text style={styles.clanName}>{currentClan?.clan_name}</Text>
 		</View>
 	);
