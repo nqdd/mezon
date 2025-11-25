709e1b7899d07acf892846119a4adc1460e63b10
sadohpv <anh.trantruong@ncc.asia>
2025-11-25 16:40:20 +0700

    fix disbale input chat when override permission

--- libs/core/src/lib/chat/contexts/ChatContext.tsx
+++ libs/core/src/lib/chat/contexts/ChatContext.tsx
@@ -95,7 +95,7 @@ import {
 	webhookActions
 } from '@mezon/store';
 import { useMezon } from '@mezon/transport';
-import type { EOverriddenPermission, IMessageSendPayload, IUserProfileActivity, NotificationCategory } from '@mezon/utils';
+import type { IMessageSendPayload, IUserProfileActivity, NotificationCategory } from '@mezon/utils';
 import {
 	ADD_ROLE_CHANNEL_STATUS,
 	AMOUNT_TOKEN,
@@ -103,6 +103,7 @@ import {
 	EEventAction,
 	EEventStatus,
 	EMuteState,
+	EOverriddenPermission,
 	ERepeatType,
 	EUserStatus,
 	IMessageTypeCallLog,
@@ -1848,7 +1849,7 @@ const ChatContextProvider: React.FC<ChatContextProviderProps> = ({ children }) =
 					...(userPermission.default_permissions?.map((perm) => ({
 						id: perm.permission_id as string,
 						slug: perm.slug as EOverriddenPermission,
-						active: perm.type || 0
+						active: perm.slug === EOverriddenPermission.sendMessage ? 1 : 0
 					})) || [])
 				];
 				dispatch(
