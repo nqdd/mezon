c33b9d410ca90169f0c13ffc15f1421139fa5f6e
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2025-12-05 16:45:29 +0700

    add: logic mark as read

--- apps/mobile/src/app/screens/home/homedrawer/components/ChannelList/RemoveNotificationListener/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/ChannelList/RemoveNotificationListener/index.tsx
@@ -1,11 +1,28 @@
+import { ActionEmitEvent } from '@mezon/mobile-components';
 import { selectCurrentChannel, selectDmGroupCurrentId } from '@mezon/store-mobile';
 import notifee from '@notifee/react-native';
-import { memo, useEffect } from 'react';
+import { memo, useCallback, useEffect } from 'react';
+import { DeviceEventEmitter } from 'react-native';
 import { useSelector } from 'react-redux';
 
 const RemoveNotificationListener = () => {
 	const currentChannel = useSelector(selectCurrentChannel);
 	const currentDirectId = useSelector(selectDmGroupCurrentId);
+
+	const triggerRemoveByChannelId = useCallback(async (channelId: string) => {
+		try {
+			const displayedNotifications = await notifee.getDisplayedNotifications();
+			const notificationsToRemove = displayedNotifications.filter((item) => item.notification?.data?.channel === channelId);
+
+			for (const notification of notificationsToRemove) {
+				if (notification.id) {
+					await notifee.cancelDisplayedNotification(notification.id);
+				}
+			}
+		} catch (error) {
+			console.error('Error removing notifications:', error);
+		}
+	}, []);
 
 	useEffect(() => {
 		const removeNotificationsByChannelId = async () => {
@@ -14,23 +31,23 @@ const RemoveNotificationListener = () => {
 			if (!channelId) {
 				return;
 			}
-
-			try {
-				const displayedNotifications = await notifee.getDisplayedNotifications();
-				const notificationsToRemove = displayedNotifications.filter((item) => item.notification?.data?.channel === channelId);
-
-				for (const notification of notificationsToRemove) {
-					if (notification.id) {
-						await notifee.cancelDisplayedNotification(notification.id);
-					}
-				}
-			} catch (error) {
-				console.error('Error removing notifications:', error);
-			}
+			await triggerRemoveByChannelId(channelId);
 		};
 
 		removeNotificationsByChannelId();
-	}, [currentChannel?.channel_id, currentDirectId]);
+	}, [currentChannel?.channel_id, currentDirectId, triggerRemoveByChannelId]);
+
+	useEffect(() => {
+		const onRemoveNotifyListener = DeviceEventEmitter.addListener(ActionEmitEvent.ON_REMOVE_NOTIFY_BY_CHANNEL_ID, ({ channelId = '' }) => {
+			if (channelId) {
+				triggerRemoveByChannelId(channelId);
+			}
+		});
+		return () => {
+			onRemoveNotifyListener.remove();
+		};
+	}, [triggerRemoveByChannelId]);
+
 	return null;
 };
 
