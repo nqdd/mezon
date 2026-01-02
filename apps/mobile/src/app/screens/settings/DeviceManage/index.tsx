0142000261eb3b65d00cc7dbb5cc3bdce56c87ef
hoangtranlehuy <hoang.tranlehuy@ncc.asia>
2026-01-02 16:32:33 +0700

    260102-MOBILE-Feat show list device mobile

---
+++ apps/mobile/src/app/screens/settings/DeviceManage/index.tsx
@@ -0,0 +1,39 @@
+import { useTheme } from '@mezon/mobile-ui';
+import type { IDevice } from '@mezon/store-mobile';
+import { fetchListLoggedDevices, selectAllDevices, selectDevicesLoadingStatus, useAppDispatch, useAppSelector } from '@mezon/store-mobile';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { FlatList, Text, View } from 'react-native';
+import { Flow } from 'react-native-animated-spinkit';
+import DeviceItem from './DeviceItem';
+import { style } from './styles';
+
+const DeviceManage = () => {
+	const { themeValue } = useTheme();
+	const styles = style(themeValue);
+	const { t } = useTranslation(['setting']);
+	const loadingStatus = useAppSelector(selectDevicesLoadingStatus);
+	const allDevices = useAppSelector(selectAllDevices);
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		dispatch(fetchListLoggedDevices());
+	}, []);
+
+	const renderItem = ({ item }: { item: IDevice }) => <DeviceItem item={item} />;
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.description}>{t('deviceSettings.description1')}</Text>
+			<Text style={styles.description}>{t('deviceSettings.description2')}</Text>
+			{loadingStatus === 'loading' && allDevices.length === 0 && (
+				<View style={styles.containerLoading}>
+					<Flow color={themeValue.textDisabled} />
+				</View>
+			)}
+			<FlatList data={allDevices} keyExtractor={(item) => item?.device_id} renderItem={renderItem} />
+		</View>
+	);
+};
+
+export default DeviceManage;
---
+++ apps/mobile/src/app/screens/settings/DeviceManage/styles.ts
@@ -0,0 +1,24 @@
+import type { Attributes } from '@mezon/mobile-ui';
+import { size } from '@mezon/mobile-ui';
+import { StyleSheet } from 'react-native';
+
+export const style = (colors: Attributes) =>
+	StyleSheet.create({
+		container: {
+			flex: 1,
+			backgroundColor: colors.primary,
+			paddingHorizontal: size.s_16
+		},
+		description: {
+			marginBottom: size.s_8,
+			color: colors.text,
+			fontSize: size.s_12,
+			lineHeight: size.s_15
+		},
+		containerLoading: {
+			flex: 1,
+			justifyContent: 'center',
+			alignItems: 'center',
+			paddingTop: size.s_10
+		}
+	});
