8ad2e9fd1225e8d7a28ef5c70fe54d7ae814557b
kind6688 <thin.tranhoang@ncc.asia>
2026-03-03 08:59:42 +0700

    list-thread-flashes-when-press

--- libs/core/src/lib/chat/contexts/ChatContext.tsx
+++ libs/core/src/lib/chat/contexts/ChatContext.tsx
@@ -1041,17 +1041,17 @@ const ChatContextProvider: React.FC<ChatContextProviderProps> = ({ children, isM
 							const thread: ThreadsEntity = {
 								id: channel.id,
 								channel_id: channel_desc.channel_id,
-								active: 1,
+								active: ThreadStatus.joined,
 								channel_label: channel_desc.channel_label,
 								clan_id: channel_desc.clan_id || (clanId as string),
 								parent_id: channel_desc.parent_id,
+								channel_private: channel_desc.channel_private,
 								creator_id: caller?.user_id || '',
 								last_sent_message: {
-									timestamp_seconds: userAdds.create_time_second
+									timestamp_seconds: userAdds.create_time_second || Date.now() / 1000
 								},
 								type: channel_desc.type
 							};
-
 							dispatch(
 								threadsActions.addThreadToCached({
 									channelId: channel.parent_id || '',
@@ -1464,6 +1464,25 @@ const ChatContextProvider: React.FC<ChatContextProviderProps> = ({ children, isM
 					clan_id: channelCreated.clan_id
 				};
 				dispatch(listChannelRenderAction.addThreadToListRender({ clanId: channelCreated?.clan_id as string, channel: thread }));
+
+				if (channelCreated.channel_private === ChannelStatusEnum.isPrivate) {
+					const privateThread: ThreadsEntity = {
+						...channelCreated,
+						id: channelCreated.channel_id,
+						type: channelCreated.channel_type,
+						last_sent_message: {
+							sender_id: channelCreated.creator_id,
+							timestamp_seconds: Date.now() / 1000
+						},
+						active: ThreadStatus.joined
+					};
+					dispatch(
+						threadsActions.addThreadToCached({
+							channelId: channelCreated.parent_id,
+							thread: privateThread
+						})
+					);
+				}
 			}
 
 			if (channelCreated.channel_private === 1 && channelCreated.channel_type === ChannelType.CHANNEL_TYPE_CHANNEL) {
@@ -2973,3 +2992,4 @@ const ChatContextConsumer = ChatContext.Consumer;
 ChatContextProvider.displayName = 'ChatContextProvider';
 
 export { ChatContext, ChatContextConsumer, ChatContextProvider, MobileEventEmitter };
+
