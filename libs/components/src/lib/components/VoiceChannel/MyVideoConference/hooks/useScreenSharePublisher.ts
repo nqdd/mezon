103ea84918a40f5a74ea3f55f74dff8373f4e674
Nhan Nguyen <nguyentran@ncc.asia>
2026-03-16 07:47:40 +0700

    enable simulcast

--- libs/components/src/lib/components/VoiceChannel/MyVideoConference/hooks/useScreenSharePublisher.ts
+++ libs/components/src/lib/components/VoiceChannel/MyVideoConference/hooks/useScreenSharePublisher.ts
@@ -82,14 +82,14 @@ export const useScreenSharePublisher = (room?: Room | null) => {
 				const videoPublication = await room.localParticipant.publishTrack(videoTrack, {
 					name: 'screen-share',
 					source: Track.Source.ScreenShare,
-					simulcast: false,
+					simulcast: true,
 					videoCodec: 'vp8',
 					videoEncoding: {
 						maxBitrate: 4_000_000,
 						maxFramerate: 60
 					},
-					degradationPreference: 'maintain-framerate',
-					//dtx: true,
+					degradationPreference: 'maintain-resolution',
+					dtx: true,
 					screenShareSimulcastLayers: [
 						// 360p
 						{
