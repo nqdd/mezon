b2c8f1103352914f792bbbb3ae6cb9dbed22a1a3
Nguyen Luc <nguyentatluc1442003@gmail.com>
2026-03-17 15:10:24 +0700

    fix UI bug embed

--- libs/components/src/lib/components/EmbedMessage/components/EmbedImage.tsx
+++ libs/components/src/lib/components/EmbedMessage/components/EmbedImage.tsx
@@ -7,20 +7,14 @@ interface EmbedImageProps {
 	height?: number;
 }
 
-export function EmbedImage({ url, width, height }: EmbedImageProps) {
+export function EmbedImage({ url }: EmbedImageProps) {
 	const [showPreview, closePreview] = useModal(() => {
 		return <ImagePreview imageUrl={url} onClose={closePreview} />;
 	});
 
 	return (
-		<div className="mt-2 rounded overflow-hidden">
-			<img
-				src={url}
-				style={{ width: width || 'auto', height: height || 'auto' }}
-				alt=""
-				className="w-full h-auto max-w-full cursor-pointer"
-				onClick={showPreview}
-			/>
+		<div className="mt-2 rounded overflow-hidden max-w-full">
+			<img src={url} alt="" className="w-full h-auto max-w-full object-contain cursor-pointer" onClick={showPreview} />
 		</div>
 	);
 }
