15499397984f66921f797537e25d5884414967d9
kind6688 <thin.tranhoang@ncc.asia>
2026-04-10 08:51:25 +0700

    fix-jump-to-newthread

--- libs/store/src/lib/threads/threads.slice.ts
+++ libs/store/src/lib/threads/threads.slice.ts
@@ -94,9 +94,9 @@ export const fetchThreadsCached = async (
 
 	const shouldForceCall = shouldForceApiCall(apiKey, channelData.cache, noCache);
 
-	if (!shouldForceCall && channelData) {
+	if (!shouldForceCall && channelData?.ids?.length > 0) {
 		return {
-			channeldesc: channelData || [],
+			channeldesc: threadsAdapter.getSelectors().selectAll(channelData),
 			fromCache: true,
 			time: channelData.cache?.lastFetched || Date.now()
 		};
@@ -133,7 +133,11 @@ const updateCacheOnThreadCreation = createAsyncThunk(
 	}
 );
 
-const mapToThreadEntity = (threads: ApiChannelDescription[]) => {
+const mapToThreadEntity = (threads: ApiChannelDescription[] | ThreadsEntity[] | undefined | null) => {
+	if (!Array.isArray(threads)) {
+		return [];
+	}
+
 	return threads.map((thread) => ({
 		...thread,
 		id: thread.channel_id as string
@@ -205,7 +209,6 @@ export const fetchThread = createAsyncThunk('threads/fetchThread', async ({ chan
 			undefined,
 			Boolean(noCache)
 		);
-
 		if (!response.channeldesc) {
 			return {
 				channelId,
@@ -227,9 +230,7 @@ export const fetchThread = createAsyncThunk('threads/fetchThread', async ({ chan
 });
 
 const getInitialChannelState = () => {
-	return {
-		threads: threadsAdapter.getInitialState()
-	};
+	return threadsAdapter.getInitialState();
 };
 
 export const initialThreadsState: ThreadsState = threadsAdapter.getInitialState({
